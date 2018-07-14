a802f25d25f813127e6fd650bc3df4df0c355b42
Michael Hofstadter <31392780+mthofstadter@users.noreply.github.com>
2018-07-13 21:19:52 -0500

    wow that plus sign is sick
    wtf is going on

--- cue/cue/ViewController.swift
+++ cue/cue/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet var pinnedButtons: [UIButton]!
+    
+    
+    @IBAction func handleSelection(_ sender: UIButton) {
+        pinnedButtons.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+            button.isHidden = !button.isHidden
+            self.view.layoutIfNeeded()
+            
+            })
+        }
 
+    }
+    @IBOutlet weak var upcomingTable: UITableView!
+    
 
+    @IBAction func upcomingTab(_ sender: UIButton) {
+        upcomingTable.isHidden = !upcomingTable.isHidden
+    }
+    
 }
+
 
